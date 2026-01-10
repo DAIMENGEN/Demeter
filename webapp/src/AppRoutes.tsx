bdcff46879b014d32d7f8e2c6049f18eac3a00da
mengen.dai <mengen.dai@outlook.com>
2026-01-10 21:46:22 +0800

    add registration page; implement form, validation, and navigation to login

--- webapp/src/AppRoutes.tsx
+++ webapp/src/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import {LoginPage} from "@Webapp/pages/login-page/login-page.tsx";
+import {RegisterPage} from "@Webapp/pages/register-page/register-page.tsx";
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<LoginPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
         </Routes>
     )
 }
